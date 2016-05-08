efdfeef88ff39df779734d45fd6c4b091a99b3e4
shereifhawary@gmail.com <xwwxA1s2d3f4>
2016-05-08 13:41:06 +0200

    adding step 2 before updates

--- src/main/java/com/orange/induction/App.java
+++ src/main/java/com/orange/induction/App.java
@@ -5,6 +5,8 @@ package com.orange.induction;
  * [1 Comments BAD]
  */
 
+import java.util.Arrays;
+
 /**
  * [1 Comments GOOD]
  * @Auther: Shereif hawary
@@ -24,6 +26,7 @@ public class App {
     public static void main(String[] args) {
         App app = new App();
         app.printWelcomeMessageToUsers("Hello World!");
+        app.buildAndCompileAndSortEntitiesWithOrderByStatus();
     }
 
     /**
@@ -33,6 +36,59 @@ public class App {
     public void printWelcomeMessageToUsers(String message) {
         System.out.println(message);
     }
+
+
+
+
+
+    // [2 long Method]
+    public boolean buildAndCompileAndSortEntitiesWithOrderByStatus() {
+        int buildmode = 1;
+        StringBuilder sb = new StringBuilder();
+        sb.append("========================").append("!!");
+        for (int i = 0 ; i < 10 ; i++) {
+            sb.append("adding step ").append(i).append("!!");
+        }
+
+        if(sb.capacity() < 20) {
+            System.err.print("OPPS !!");
+            return false;
+        }
+
+        if(sb.length() > 30) {
+            buildmode = 2;
+        } else {
+            buildmode = 3;
+        }
+
+        sb.append("========================").append("!!");
+        //sb.append(buildmode);
+
+        String qwe = sb.toString();
+        if(buildmode == 2) {
+            sb.delete(0,sb.length());
+            sb.append(qwe.replace("=", "-"));
+        }
+        sb.append("compiled");
+        //sb.append("!!");
+
+
+        String[] qwe2 = sb.toString().split("\\!\\!");
+        sb.delete(0,sb.length());
+        Arrays.sort(qwe2);
+        if(qwe2.length == 0) {
+            System.err.print("oops can't sort and order by");
+            return false;
+        }
+
+        for(int i = 0 ; i < qwe2.length ; i++) {
+            sb.append(qwe2[i]).append("\n");
+        }
+
+        System.out.println(sb);
+        return true;
+
+    }
 }
 
 /**
@@ -41,4 +97,9 @@ public class App {
  *              Can you refactor the code so the comments aren't required?
  *              And remember, you're writing comments for people, not machines.
  *
+ * [2 Long Method] 	All other things being equal,
+ *                  a shorter method is easier to read,
+ *                  easier to understand,
+ *                  and easier to troubleshoot.
+ *                  Refactor long methods into smaller methods if you can.
  */

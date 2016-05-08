87d3cb5176269db305db7e1cc4892335ce7aa455
shereifhawary@gmail.com <xwwxA1s2d3f4>
2016-05-08 13:47:08 +0200

    adding step 2 after updates

--- src/main/java/com/orange/induction/App.java
+++ src/main/java/com/orange/induction/App.java
@@ -9,6 +9,7 @@ import java.util.Arrays;
 
 /**
  * [1 Comments GOOD]
+ *
  * @Auther: Shereif hawary
  * @since: v1.5
  * app class the main brain of the project, all the dependence are covered by maven dependence and all the logic related
@@ -18,11 +19,12 @@ public class App {
     /**
      * [1 Comments BAD]
      * this is the main function
+     *
      * @param args
      */
 
-     //[1 Comments GOOD]
-     //DO we need a documentation for Main function
+    //[1 Comments GOOD]
+    //DO we need a documentation for Main function
     public static void main(String[] args) {
         App app = new App();
         app.printWelcomeMessageToUsers("Hello World!");
@@ -31,6 +33,7 @@ public class App {
 
     /**
      * [1 comments] Print welcome message to the user
+     *
      * @param message
      */
     public void printWelcomeMessageToUsers(String message) {
@@ -38,68 +41,92 @@ public class App {
     }
 
 
-
-
-
-    // [2 long Method]
-    public boolean buildAndCompileAndSortEntitiesWithOrderByStatus() {
+    public StringBuilder build() {
         int buildmode = 1;
         StringBuilder sb = new StringBuilder();
         sb.append("========================").append("!!");
-        for (int i = 0 ; i < 10 ; i++) {
+        for (int i = 0; i < 10; i++) {
             sb.append("adding step ").append(i).append("!!");
         }
 
-        if(sb.capacity() < 20) {
+        if (sb.capacity() < 20) {
             System.err.print("OPPS !!");
-            return false;
+            return null;
         }
 
-        if(sb.length() > 30) {
+        if (sb.length() > 30) {
             buildmode = 2;
         } else {
             buildmode = 3;
         }
 
-        sb.append("========================").append("!!");
-        //sb.append(buildmode);
+        sb.append("========================").append(buildmode).append("!!");
+        return sb;
+    }
 
+    public StringBuilder compile(StringBuilder sb1) {
+
+        StringBuilder sb = new StringBuilder(sb1);
+        //sb.append(buildmode);
         String qwe = sb.toString();
-        if(buildmode == 2) {
-            sb.delete(0,sb.length());
-            sb.append(qwe.replace("=", "-"));
-        }
+
+        sb.delete(0, sb.length());
+        sb.append(qwe.replace("=", "-"));
+
         sb.append("compiled");
         //sb.append("!!");
+        return sb;
+    }
 
+    // [2 long Method]
+    public boolean buildAndCompileAndSortEntitiesWithOrderByStatus() {
 
-        String[] qwe2 = sb.toString().split("\\!\\!");
-        sb.delete(0,sb.length());
-        Arrays.sort(qwe2);
-        if(qwe2.length == 0) {
-            System.err.print("oops can't sort and order by");
+        StringBuilder sb = this.build();
+        if (sb == null) {
+            return false;
+        }
+        sb = this.compile(sb);
+        if (sb == null) {
             return false;
         }
 
-        for(int i = 0 ; i < qwe2.length ; i++) {
-            sb.append(qwe2[i]).append("\n");
+        sb = this.sortAndOrderBy(sb);
+        if (sb == null) {
+            return false;
         }
-
         System.out.println(sb);
         return true;
 
     }
+
+    private StringBuilder sortAndOrderBy(StringBuilder sb1) {
+        StringBuilder sb = new StringBuilder(sb1);
+        String[] qwe2 = sb.toString().split("\\!\\!");
+        sb.delete(0, sb.length());
+        Arrays.sort(qwe2);
+        if (qwe2.length == 0) {
+            System.err.print("oops can't sort and order by");
+            return null;
+        }
+
+        for (int i = 0; i < qwe2.length; i++) {
+            sb.append(qwe2[i]).append("\n");
+        }
+        return sb;
+    }
+
+
 }
 
 /**
  * [1 Comments]	There's a fine line between comments that illuminate and comments that obscure.
- *              Are the comments necessary? Do they explain "why" and not "what"?
- *              Can you refactor the code so the comments aren't required?
- *              And remember, you're writing comments for people, not machines.
- *
+ * Are the comments necessary? Do they explain "why" and not "what"?
+ * Can you refactor the code so the comments aren't required?
+ * And remember, you're writing comments for people, not machines.
+ * <p>
  * [2 Long Method] 	All other things being equal,
- *                  a shorter method is easier to read,
- *                  easier to understand,
- *                  and easier to troubleshoot.
- *                  Refactor long methods into smaller methods if you can.
+ * a shorter method is easier to read,
+ * easier to understand,
+ * and easier to troubleshoot.
+ * Refactor long methods into smaller methods if you can.
  */
